c6f9d07d1cccce51d0c69ec226ea49f18f660352
KSRATHORE761 <rathore.kuldeep761@gmail.com>
2025-06-11 12:55:02 +0530

    first commit

---
+++ Movie.API/Controllers/MoviesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+using Movies.Application.Models;
+using Movies.API.Mapping;
+using Movies.API;
+
+namespace Movie.API.Controllers
+{
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly IMovieRepository _movieRepository;
+        public MoviesController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        [HttpPost(ApiEndpoints.Movies.Create)]
+        public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+        {
+
+            var movie = request.MapToMovie();
+
+            await _movieRepository.CreateAsync(movie);
+            return CreatedAtAction(nameof(Get), new { id = movie.Id }, movie);
+        }
+        [HttpGet(ApiEndpoints.Movies.Get)]
+        public async Task<IActionResult> Get([FromRoute] Guid id)
+        {
+            var movie = await _movieRepository.GetByIdAsync(id);
+            if(movie is null)
+            {
+                return NotFound();
+            }
+            var response = movie.MapToResponse();
+            return Ok(response);
+        }
+        [HttpGet(ApiEndpoints.Movies.GetAll)]
+        public async Task<IActionResult> GetAll()
+        {
+            var movies = await _movieRepository.GetAllAsync();
+            var response = ;
+            return Ok(response);
+        }
+        [HttpPut(ApiEndpoints.Movies.Update)]
+        public async Task<IActionResult> Update([FromBody]UpdateMovieRequest request, [FromRoute]Guid id)
+        {
+            var movie = request.MapToMovie(id);
+            var updated = await _movieRepository.UpdateAsync(movie);
+            if (!updated)
+            {
+                return NotFound();
+            }
+            var response = movie.MapToResponse();
+            return Ok(response);
+        }
+    }
+}
---
+++ Movie.API/Mapping/ContractMapping.cs
@@ -0,0 +1,51 @@
+﻿using Movies.Application.Models;
+using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+
+namespace Movies.API.Mapping
+{
+    public static class ContractMapping
+    {
+        public static Movies.Application.Models.Movie MapToMovie(this CreateMovieRequest request)
+        {
+            return new Movies.Application.Models.Movie
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title,
+                YearOfRelease = request.YearOfRelease,
+                Genres = request.Genres.ToList(),
+
+            };
+        }
+
+        public static MovieResponse MapToResponse(this Movies.Application.Models.Movie movie)
+        {
+            return new MovieResponse
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                YearOfRelease = movie.YearOfRelease,
+                Genres = movie.Genres
+            };
+        }
+
+        public static MoviesResponse MapToResponse(this IEnumerable<Movies.Application.Models.Movie> movies)
+        {
+            return new MoviesResponse
+            {
+                Items = (IEnumerable<MoviesResponse>)movies.Select(MapToResponse)
+            };
+        }
+        public static Movies.Application.Models.Movie MapToMovie(this UpdateMovieRequest request, Guid id)
+        {
+            return new Movies.Application.Models.Movie
+            {
+                Id = id,
+                Title = request.Title,
+                YearOfRelease = request.YearOfRelease,
+                Genres = request.Genres.ToList(),
+
+            };
+        }
+    }
+}
